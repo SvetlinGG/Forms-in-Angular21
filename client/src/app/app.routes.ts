63f3615891fbc5430a3628735b4105d2e037e693
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-29 22:28:57 +0200

    make header template

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'header', component: HeaderComponent},
     { path: 'login', component: LoginComponent }
 ];
