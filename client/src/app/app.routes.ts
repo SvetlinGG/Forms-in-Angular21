8ba6816596c7404e9d3787bd41ae386f3983fe87
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-30 03:11:27 +0200

    routing to home component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HeaderComponent } from './pages/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
     { path: 'header', component: HeaderComponent},
     { path: 'login', component: LoginComponent }
 ];
