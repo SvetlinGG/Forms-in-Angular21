949ab7853796901bd344542d5379cf10dff9a5c8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-01 00:43:11 +0200

    update css file of login form

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -5,17 +5,22 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  @ViewChild('loginForm') form: NgForm | undefined;
+  @ViewChild('registerForm') form: NgForm | undefined;
 
-  submitHandler(nameInput: HTMLInputElement, emailInput: HTMLInputElement){
+  formSubmitHandler(nameInput: HTMLInputElement, emailInput: HTMLInputElement){
+
+    const form = this.form
+
+
     console.log(nameInput);
     console.log(emailInput);
     
+    form?.reset()
 
   }
 
