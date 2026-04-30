bc899731c8ca9fd79164b74503f9490d5d5e30b8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-30 13:14:28 +0200

    setup register form with

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  submitHandler(nameInput: HTMLLabelElement){
+    console.log();
+    
+
+  }
 
 }
