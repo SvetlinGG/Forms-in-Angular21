7d7f489ffcf7f888e12b699b36dacf1db10ed83b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-01 22:22:03 +0200

    setup auth routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,1 +1,15 @@
 import { Router } from "express";
+import { login, register } from "../controllers/authControllers";
+
+
+const router = Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+router.post('/register', (req, res) => {
+    console.log('REGISTER ROUTE HIT');
+    res.status(201).json({ message: 'register route works'})
+});
+
+export default router;
