be7223456087c31dee67533fe953344afb339ab6
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-01 22:33:55 +0200

    set the server methods

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import authRoutes from './routes/authRoutes.js'
 
 
 const app = express();
@@ -10,7 +11,20 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use('/api/auth', )
+app.use('/api/auth', authRoutes);
+
+app.use((req, res, next) => {
+    console.log('REQ:', req.method, req.url);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('ROOT OK');
+});
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'API works'})
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
