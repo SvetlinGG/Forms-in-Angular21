486deffcc9ec225f116219f87fdc0dfde0f1c5c8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-01 22:17:26 +0200

    authentication user

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -2,3 +2,74 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/User.js';
 
+const generateToken = (user) => {
+    return jwt.sign(
+        {
+            _id: user._id,
+            email: user.email,
+            username: user.username
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '2h'}
+    );
+};
+
+export const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            return res.status(400).json({ message: 'User already exists'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        const token = generateToken(user);
+
+        res.status(201).json({
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            accessToken: token
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message || 'Register failed'})
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({email});
+
+        if (!user){
+            return res.status(400).json({ message: 'Invalid email or password'});
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword){
+            return res.status(400).json({ message: 'Invalid email or password'});
+        }
+
+        const token = generateToken(user);
+
+        res.json({
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            accessToken: token
+        });
+    } catch (error) {
+        res.status(500).json({message: error.message || 'Login failed'})
+    }
+};
