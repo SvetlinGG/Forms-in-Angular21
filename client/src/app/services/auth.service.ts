67291c367b8138e5015adde54fd505d8ee358e78
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-03 21:56:06 +0200

    connect service with User model

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,11 +1,22 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+import { AuthUser } from '../models/auth-user.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
 
   private apiUrl = 'http://localhost:27017/api/chat';
 
-  user = signal
+  user = signal<AuthUser | null>(this.getUserFromStorage());
+  isLoggedIn = computed(() => !!this.user())
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
+
+
+  private getUserFromStorage(): AuthUser | null{
+    const rawUser = localStorage.getItem('user');
+    return rawUser ? JSON.parse(rawUser) : null;
+  }
 }
