9427acb7d137ab886ae69ceb50afce89f85a7705
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-03 02:32:20 +0200

    authentication middleware

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+function authMiddleware(req, res, next){
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).json({ message: 'No authorization header'})
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token){
+        return res.status(401).json({ message: 'Np token provided'});
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = {...decodedToken, id: decodedToken._id || decodedToken.id };
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token'})
+    }
+}
+
+export default  authMiddleware;
