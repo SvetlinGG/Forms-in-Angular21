88fd89f354b5ed100b155ac7db8bad1c72c1574e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-03 18:09:10 +0200

    post requests

--- server/src/routes/commentRoutes.js
+++ server/src/routes/commentRoutes.js
@@ -27,3 +27,59 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+
+    try {
+        const comment = await Comment.findById(req.params.id)
+            .populate('owner', 'username email')
+            .populate('comments.user', 'username email');
+
+        if(!comment) return res.status(404).json({ message: 'Comment not found'});
+        res.json(comment)
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to fetch comment details'})
+    }
+});
+
+router.post('/', authMiddleware, async (req, res) => {
+    try {
+        const comment = await Comment.create({
+            ...req.body,
+            owner: req.user.id
+        });
+
+        res.status(201).json(comment)
+    } catch (error) {
+        res.status(400).json({message: 'Failed to create comment'})
+    }
+});
+
+router.post('/:id/like', authMiddleware, async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.id)
+
+        if(!comment){
+            return res.status(404).json({message: 'Comment not found'})
+        }
+
+        const userId = req.user.id;
+        const alreadyLiked = comment.likes.some((id) => id.toString() === userId);
+
+        if(alreadyLiked){
+            comment.likes = comment.likes.filter((id) => id.toString() !== userId);
+        }else{
+            comment.likes.push(userId);
+        }
+
+        await comment.save();
+
+        const updatedComment = await Comment.findById(req.params.id)
+            .populate('owner', 'username email')
+            .populate('comments.user', 'username email');
+
+        res.json(updatedComment)
+        } catch (error) {
+            res.status(500).json({ message: 'Failed to update likes'})
+    }
+});
+
